e5f69d4dfecc7f9fde19c8287164653e27d8e9c7
ha <ha2896@naver.com>
2020-04-26 17:05:22 +0900

    0426

--- menu/src/main/java/com/example/project/MainActivity.java
+++ menu/src/main/java/com/example/project/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView wasabi = findViewById(R.id.wasabi);
+        wasabi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WasabiActivibity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
