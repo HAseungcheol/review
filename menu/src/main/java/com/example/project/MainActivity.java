2ba1344101ddc76e6d3847bf2422613430c6dc80
ha <ha2896@naver.com>
2020-04-26 18:07:40 +0900

    0426 오후 6시

--- menu/src/main/java/com/example/project/MainActivity.java
+++ menu/src/main/java/com/example/project/MainActivity.java
@@ -14,11 +14,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //각 레이아웃에 창 전환 Event
         ImageView wasabi = findViewById(R.id.wasabi);
         wasabi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, WasabiActivibity.class);
+                Intent intent = new Intent(MainActivity.this, WasabiActivity.class);
+                startActivity(intent);
+            }
+        });
+        ImageView tuna = findViewById(R.id.tuna);
+        tuna.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TunaActivity.class);
+                startActivity(intent);
+            }
+        });
+        ImageView egg = findViewById(R.id.egg);
+        egg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EggActivity.class);
+                startActivity(intent);
+            }
+        });
+        ImageView pork = findViewById(R.id.pork);
+        pork.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PorkActivity.class);
                 startActivity(intent);
             }
         });
