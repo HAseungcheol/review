1842204382865655357b292abde89daaea6a03ca
ha <ha2896@naver.com>
2020-05-04 14:50:14 +0900

    0504 오후 2시 50분

--- menu/src/main/java/com/example/project/TunaActivity.java
+++ menu/src/main/java/com/example/project/TunaActivity.java
@@ -1,14 +1,31 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TunaActivity extends AppCompatActivity {
+    ListView comment_list;
+    TextView time_text;
+    EditText comment_edit;
+    ImageView jrv_image_img;
+    Comment_Adapter ca;
+    ArrayList<Comment_Item> c_arr = new ArrayList<Comment_Item>();
+    View header,footer;
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tuna);
+        init();
+    }
+    public void init() {
+        comment_list = (ListView) findViewById(R.id.jrv_comment_list);
     }
 }
