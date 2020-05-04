38baf55e41b8ebe3fd29e869149e85668dd230f2
gyurim <phd9734@gmail.com>
2020-05-04 18:30:50 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	menu/src/main/java/com/example/project/MainActivity.java
    #	menu/src/main/java/com/example/project/TunaActivity.java

---
+++ menu/src/main/java/com/example/project/TunaActivity.java
@@ -0,0 +1,43 @@
+package com.example.project;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TunaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tuna);
+
+        Button material = findViewById(R.id.material2);
+        material.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setTitle("★재료 준비★")
+                        .setMessage(" 참치 1/2캔, 달걀 1개, 양파 1/2개, 깻잎 2장 김가루, 마요네즈 2~3T, 후추톡톡\n\n 장 (데리야끼소스) : 간장 3T, 맛술 1T, 청주1T 설탕 1T (또는 설탕 0.5T, 물엿 0.5T)");
+                builder.setPositiveButton("확인", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+    }
+
+}
