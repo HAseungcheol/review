4a245b8574771b25575ad4611130c025969a5c26
ha <ha2896@naver.com>
2020-05-04 17:53:12 +0900

    0504 오후 5:53분

--- menu/src/main/java/com/example/project/EggActivity.java
+++ menu/src/main/java/com/example/project/EggActivity.java
@@ -1,7 +1,11 @@
 package com.example.project;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EggActivity extends AppCompatActivity {
@@ -10,5 +14,26 @@ public class EggActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_egg);
+
+        Button button = findViewById(R.id.material3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setTitle("★재료 준비★")
+                        .setMessage(" 계란 3개,  옥수수 통조림 반컵,\n 슬라이스햄 2장, 슬라이스 치즈 1장,\n" +
+                                "마요네즈 0.5, 우유 3, 소금/후추\n" +
+                                "\n" +
+                                "1인분 기준 (우유 1 = 우유 1큰술 = 15ml)");
+                builder.setPositiveButton("확인", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
     }
 }
--- menu/src/main/java/com/example/project/WasabiActivity.java
+++ menu/src/main/java/com/example/project/WasabiActivity.java
@@ -1,7 +1,11 @@
 package com.example.project;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WasabiActivity extends AppCompatActivity {
@@ -10,5 +14,30 @@ public class WasabiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wasabi);
+
+        Button button = findViewById(R.id.material);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setTitle("★재료 준비★")
+                        .setMessage("필수재료\n" +
+                                "따뜻한 밥(1공기), 날치알(1), 김가루(적당량)\n" +
+                                "\n" +
+                                "선택재료\n" +
+                                "맛살(2쪽), 쪽파(1대)\n" +
+                                "\n" +
+                                "양념\n" +
+                                "소금(약간), 설탕(0.5), 식초(1.5), 와사비(0.5)");
+                builder.setPositiveButton("확인", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
     }
 }
