80115fd4b45252ecb89fd531d07574ee5008ad33
gyurim <phd9734@gmail.com>
2020-05-04 18:24:17 +0900

    ddd

--- menu/src/main/java/com/example/project/MainActivity.java
+++ menu/src/main/java/com/example/project/MainActivity.java
@@ -7,13 +7,14 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    List<Drawable> temp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,5 +54,7 @@ public void onClick(View v) {
         startActivity(intent);
         }
         });
+
+
     }
 }
